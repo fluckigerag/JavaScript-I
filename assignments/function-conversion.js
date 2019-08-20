6377aee73f990be215841948f9beb3d3ce498311
Angelica <agf1867@gmail.com>
2019-08-19 21:05:30 -0600

    Finished Project

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,25 +1,61 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
+// Challenge 1
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
 // myFunction();
 
+let myFunction = () => {
+    console.log("Function was invoked!");
+};
+myFunction();
+
+
+// //function declaration syntax
+// function funcName (params) {
+//     return "something";
+// }
+
+// //arrow function syntax 
+// const funcName = (params) => {
+//     return "something";
+// }
+
+
+// Challenge 2
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+let anotherFunction = (param) => {
+    return param;
+};
+anotherFunction("Example");
+console.log(anotherFunction("Example"))
+
+// Challenge 3
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => {
+    return param1 + param2;
+};
+console.log(add(1, 2))
+
+// Challenge 4
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => {
+    return param1 - param2;
+};
+console.log(subtract(1, 2))
 
 // Stretch
 
